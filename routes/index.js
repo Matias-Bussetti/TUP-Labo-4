3a6e6f8e907f4a2ee693fe513048a2adaea9466a
ChapaStone1 <Chapapr@gmail.com>
2024-09-13 22:09:57 -0300

    Arrancando primer get

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 //Rutas
 const sample = require("./sample");
+const CharactersMarvel = require("./CharactersMarvel");
 
 router.use("/sample", sample);
+router.use("/api/v1/marvel/characters", CharactersMarvel);
 router.get("*", (req, res) => {
   res.json({
     status: 404,
